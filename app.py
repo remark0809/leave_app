88247903b65778c0fd5ff76eb132cf418bf8879d
remark0809 <40047711+remark0809@users.noreply.github.com>
2024-11-22 10:13:20 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -74,5 +74,5 @@ def line_notify(message):
     requests.post(url, headers=headers, data=payload)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
